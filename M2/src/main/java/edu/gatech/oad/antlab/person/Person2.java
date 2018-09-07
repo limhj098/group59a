22b525b0b77a1b0ec239cf8ab1735e6d5f0da2c4
Ryan Gadhi <gadi@gatech.edu>
2018-09-06 23:33:31 -0400

    bug fixes in Person2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,12 +1,15 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
- * @version 1.1
+ * @author RyanGadhi
+ * @version 1.3
  */
 public class Person2 {
     /** Holds the persons real name */
@@ -30,8 +33,26 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+
+		String output = "";
+		char[] arr = input.toCharArray();
+		char[] arr2 = new char[arr.length];
+
+		Random ran = new Random();
+		ArrayList<Integer> usedIndex = new ArrayList<Integer>();
+		int currentRan= ran.nextInt(arr.length);
+
+		for(int i=0 ; i< arr.length ; i++){
+			while(usedIndex.contains(currentRan))		// if the random index has been used
+				currentRan = ran.nextInt(arr.length);	// it will not use it again
+
+			usedIndex.add(currentRan);
+			arr2[i]=arr[currentRan]; 					// building the randomized array
+		}
+		for (int i =0 ; i< arr2.length ; i++)			// converting the arr2 to a String
+			output += arr2[i];
+
+		return output;
 	}
 	/**
 	 * Return a string rep of this object
