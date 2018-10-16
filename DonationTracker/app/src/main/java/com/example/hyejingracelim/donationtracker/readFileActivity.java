12cb5f01110602cea1f9819f20d7f9c1646a3224
Jinwoo Hwang <jhwang81@gatech.edu>
2018-10-16 19:03:48 -0400

    Read File Being Worked

--- DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/readFileActivity.java
+++ DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/readFileActivity.java
@@ -4,14 +4,28 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 import java.io.FileReader;
+import java.util.List;
 
+private int key;
+private String locationName, locationType, address, city, state, type, latitude, longitude, zipCode,
+               phone, website;
 public static class readFileActivity {
     public static void readData(String file) {
         try {
             FileReader fileReader = new FileReader(file);
             CSVReader csvReader = new CSVReaderBuilder(fileReader)
-                                      .withSkipLines(1)
+                                      .withSkipLines(2)
                                       .build();
+            List<String[]> data = csvReader.readAll();
+            for (String[] row : data) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
