4a2a85ad118b5104cc346c665a1341af7cf50862
Jinwoo Hwang <jhwang81@gatech.edu>
2018-10-17 01:11:58 -0400

    Read File Java

--- DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/readFile.java
+++ DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/readFile.java
@@ -1,5 +1,5 @@
 package com.example.hyejingracelim.donationtracker;
-
+import java.io.BufferedReader;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
@@ -7,28 +7,39 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class readFile {
-    private static List<String[]> data;
+    static String line = "";
+    static String cvsSplit = ",";
+    static BufferedReader readFile = null;
+    static String[] stored;
     public static void readData() {
         try {
-            FileReader fileReader = new FileReader(new File("C:\\\\Users\\\\Jinwoo Hwang\\\\Documents\\\\group59a\\\\DonationTracker\\LocationData.csv"));
-            CSVReader csvReader = new CSVReaderBuilder(fileReader)
-                    .withSkipLines(2)
-                    .build();
-            data = csvReader.readAll();
+            readFile = new BufferedReader(new FileReader("LocationData.csv"));
+            while ((line = readFile.readLine()) != null) {
+                stored = line.split(cvsSplit);
+            }
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         catch (IOException e1) {
             e1.printStackTrace();
+        } finally {
+            if (readFile != null) {
+                try {
+                    readFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
-    public static List<String[]> getData() {
-        return data;
+    public static String[] getData() {
+        return stored;
     }
 }
 
