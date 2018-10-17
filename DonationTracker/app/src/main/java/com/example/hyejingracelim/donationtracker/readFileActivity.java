23ab6d5500f8d096a3a8b385422aec299c57325b
Jinwoo Hwang <jhwang81@gatech.edu>
2018-10-16 20:51:55 -0400

    Read File Java

--- DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/readFileActivity.java
+++ DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/readFileActivity.java
@@ -3,25 +3,48 @@ package com.example.hyejingracelim.donationtracker;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
+import java.io.File;
 import java.io.FileReader;
 import java.util.List;
 
 public class readFileActivity {
-    private int key;
-    private String locationName, locationType, address, city, state, type, latitude, longitude, zipCode,
+    private String key, locationName, locationType, address, city, state, latitude, longitude, zipCode,
             phone, website;
-    public static void readData(String file) {
+    public static void readData() {
         try {
+            File file = new File("LocationData.csv")
             FileReader fileReader = new FileReader(file);
             CSVReader csvReader = new CSVReaderBuilder(fileReader)
                                       .withSkipLines(2)
                                       .build();
             List<String[]> data = csvReader.readAll();
-            for (String[] row : data) {
-                for (String cell : row) {
-                    System.out.print(cell + "\t");
-                }
-                System.out.println();
+            for (int i = 0; i < data.size(); i++) {
+                for (int j = 0; j < 11; j++) {
+                    switch (j) {
+                        case 0: key = data.get(i);
+                                break;
+                        case 1: locationName = data.get(i);
+                                break;
+                        case 2: latitude = data.get(i);
+                                break;
+                        case 3: longitude = data.get(i);
+                                break;
+                        case 4: address = data.get(i);
+                                break;
+                        case 5: city = data.get(i);
+                                break;
+                        case 6: state = data.get(i);
+                                break;
+                        case 7: zipCode = data.get(i);
+                                break;
+                        case 8: locationType = data.get(i);
+                                break;
+                        case 9: phone = data.get(i);
+                                break;
+                        case 10: website = data.get(i);
+                                break;
+                        default: break;
+                    }
             }
         }
         catch (Exception e) {
