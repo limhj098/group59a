6136549f06f1cf098905ae517f2c0a316a409c86
Freda Chen <fredachen98@gmail.com>
2018-10-23 22:57:42 -0400

    made variables global

--- DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/itemDisplayActivity.java
+++ DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/itemDisplayActivity.java
@@ -15,6 +15,14 @@ import android.widget.ArrayAdapter;
 
 public class itemDisplayActivity extends AppCompatActivity {
 
+    private String myTime;
+    private String name;
+    private String shortDescription;
+    private String fullDescription;
+    private String location;
+    private String catagory;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +30,12 @@ public class itemDisplayActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String myTime = extras.getString("time");
-            String name = extras.getString("name");
-            String shortDescription = extras.getString("shortDescription");
-            String fullDescription = extras.getString("fullDescription");
-            String location = extras.getString("location");
-            String catagory = extras.getString("catagory");
+            myTime = extras.getString("time");
+            name = extras.getString("name");
+            shortDescription = extras.getString("shortDescription");
+            fullDescription = extras.getString("fullDescription");
+            location = extras.getString("location");
+            catagory = extras.getString("catagory");
         }
     }
 
