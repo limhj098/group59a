e23eda323c9c481cf0cad05590ce50374a1c9bd7
limhj098 <limhj098@gmail.com>
2018-11-09 14:49:52 -0500

    Finished LINT check on OptionFrag

--- DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/OptionFrag.java
+++ DonationTracker/app/src/main/java/com/example/hyejingracelim/donationtracker/OptionFrag.java
@@ -2,13 +2,17 @@ package com.example.hyejingracelim.donationtracker;
 
 
 import android.content.Intent;
+
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
+
 import android.widget.Button;
+
+
 
 
 /**
@@ -17,13 +21,8 @@ import android.widget.Button;
 public class OptionFrag extends Fragment{
 
 
-    public OptionFrag() {
-        // Required empty public constructor
-    }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_option, container, false);
@@ -33,12 +32,14 @@ public class OptionFrag extends Fragment{
         final Button search = v.findViewById(R.id.search);
         final Button map = v.findViewById(R.id.map);
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 Intent i2 = new Intent(getActivity(),DataEntryActivity.class);
                 startActivity(i2);
                 // getActivity().finish();
             }});
         button1.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 Intent i3 = new Intent(getActivity(),itemDisplayActivity.class);
                 startActivity(i3);
@@ -46,6 +47,7 @@ public class OptionFrag extends Fragment{
             }});
 
         search.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 Intent i4 = new Intent(getActivity(),SearchActivity.class);
                 startActivity(i4);
@@ -53,6 +55,7 @@ public class OptionFrag extends Fragment{
             }});
 
         map.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 Intent i5 = new Intent(getActivity(),MapsActivity.class);
                 startActivity(i5);
@@ -63,14 +66,16 @@ public class OptionFrag extends Fragment{
         return v;
     }
 
-    /**
-     * This method logs the user out of the account using an Intent
-     *
-     */
-    public void logout(View view){
-        Intent i = new Intent(getActivity(),LoginActivity.class);
-        startActivity(i);
-        getActivity().finish();
-    }
+//    /**
+//     * This method logs the user out of the account using an Intent
+//     * @param view screen for logout button
+//     */
+//    public void logout(View view){
+//        Intent i = new Intent(getActivity(),LoginActivity.class);
+//        startActivity(i);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            Objects.requireNonNull(getActivity()).finish();
+//        }
+//    }
 
 }
